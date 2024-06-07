cef056beaec8d0b9cf8cbe6f74e93fdb5d392c55
Cezary <cezary@datafreak.pl>
2024-06-07 15:50:41 +0200

    optimized for limitted api for tweet create only

--- bigquery-twitter.py
+++ bigquery-twitter.py
@@ -1,24 +1,20 @@
+import tweepy.client
 import yaml
-import os
-import tweepy
-from datetime import date
-
 
 # Define your API keys
 with open('twitter-api/secrets.yaml', 'r') as file:
     config = yaml.safe_load(file)
 
 
-#Authenticate with the API
-auth = tweepy.OAuthHandler(config['twitter_credentials']['CONSUMER_KEY'], config['twitter_credentials']['CONSUMER_SECRET'])
-auth.set_access_token(config['twitter_credentials']['ACCESS_TOKEN'], config['twitter_credentials']['ACCESS_SECRET'])
-api = tweepy.API(auth)
-print(f"Zalogowano jako : {api.verify_credentials().screen_name}")
+#Authenticate with the Clientś [with free account now you only can create tweets]
+client = tweepy.Client(
+    consumer_key=config['twitter_credentials']['CONSUMER_KEY'], consumer_secret=config['twitter_credentials']['CONSUMER_SECRET'],
+    access_token=config['twitter_credentials']['ACCESS_TOKEN'], access_token_secret=config['twitter_credentials']['ACCESS_SECRET']
+    )
 
-#Define accounts to reteive data
-# konta = ["Platforma_org", "pisorgpl", "KONFEDERACJA_", "nowePSL", "PL_2050", "__Lewica"]
+print(f"Zalogowano jako {client.get_me().data['username']}")
 
-# for konta in konta:
-#     dane = api.get_user(screen_name=konta)
-#     wiersz_z_danymi = [{"User": konta, "Followers": str(dane.followers_count)}]
-#     print(wiersz_z_danymi)
+
+# with free account now you only can create tweets
+tweet = "Post created with free API. Might delete later"
+client.create_tweet(text=tweet)
